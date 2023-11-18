9bdc033da93a95558e31b4709469ae68ecb35cc3
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2023-11-18 19:29:41 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -374,45 +374,13 @@ module.exports = A17 = async (A17, m, chatUpdate, store) => {
 
 
     //
-    for (let anju of kaiaudio){
-      if (budy === anju){
-        result = fs.readFileSync(`./Assets/audio/${anju}.mp3`)
-        A17.sendMessage(m.chat, { audio: result, mimetype: 'audio/mp4', ptt: true }, { quoted: m })     
-        }
-    }
+    
 
 
 
     //----------------------------------------------------------------------------------------------------------------//
 
   //don't edit this part.
-const formatTime = (seconds) => {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const secs = seconds % 60;
-  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
-};
-
-function updateStatus() {
-  const uptimeInSeconds = Math.floor(process.uptime());
-  const uptimeFormatted = formatTime(uptimeInSeconds);
-
-
-  function _0x582b(_0xabb6f8,_0x12cdd8){const _0x58e890=_0x58e8();return _0x582b=function(_0x582b90,_0x4387b3){_0x582b90=_0x582b90-0x189;let _0x932613=_0x58e890[_0x582b90];return _0x932613;},_0x582b(_0xabb6f8,_0x12cdd8);}function _0x58e8(){const _0x109554=['12896370RDSmnX','3BgvPel','189HbmdoW','18854HvEPNh','11TZHUID','9125326EcyeIg','464328lPaAMf','3400722cbWEOK','2263175KIczdo','12TaHNqM','2521564eqJRHK'];_0x58e8=function(){return _0x109554;};return _0x58e8();}(function(_0x429d7b,_0x532ab5){const _0x527567=_0x582b,_0x130eb4=_0x429d7b();while(!![]){try{const _0x75c57a=-parseInt(_0x527567(0x18b))/0x1+-parseInt(_0x527567(0x192))/0x2*(-parseInt(_0x527567(0x189))/0x3)+parseInt(_0x527567(0x191))/0x4*(-parseInt(_0x527567(0x190))/0x5)+-parseInt(_0x527567(0x18f))/0x6+parseInt(_0x527567(0x18d))/0x7+parseInt(_0x527567(0x18e))/0x8*(-parseInt(_0x527567(0x18a))/0x9)+parseInt(_0x527567(0x193))/0xa*(parseInt(_0x527567(0x18c))/0xb);if(_0x75c57a===_0x532ab5)break;else _0x130eb4['push'](_0x130eb4['shift']());}catch(_0x19ea04){_0x130eb4['push'](_0x130eb4['shift']());}}}(_0x58e8,0xa8dae));const status='\x0a\x20\x20ㅤㅤ👨‍💻ㅤㅤ〘\x20𝙳𝙳𝙴𝚅\x20𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝙻\x20𝙴𝙳𝙸𝚃𝙸𝙾𝙽\x20〙ㅤㅤ👩‍💻ㅤㅤㅤㅤ\x0a\x20\x20\x20\x20ㅤㅤㅤ            〘ㅤ\x20𝙰𝚄𝚃𝙾\x20𝚄𝙿𝚃𝙸𝙼𝙴:\x20'+uptimeFormatted+'ㅤ〙';
-
-  A17.setStatus(status); // Set the status using A17.setStatus or your equivalent method
-
-  // Update the status randomly within 5 minutes (300000 milliseconds)
-  const randomTime = Math.floor(Math.random() * 300000) + 1000; // don't edit.
-  setTimeout(updateStatus, randomTime);
-}
-
-// Initial call to start the random status updates
-updateStatus();
-
-            
-
-
 
 
 	this.game = this.game ? this.game : {}
